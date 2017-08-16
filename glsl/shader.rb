dbb8a7c84b11ad98faf1ec3376aad2ee52e52bb1
arthurnum <arthurnum@gmail.com>
2017-08-16 16:59:46 +0300

    show shader error

--- glsl/shader.rb
+++ glsl/shader.rb
@@ -9,6 +9,26 @@ module GLSL
       @id = glCreateShader(TYPES[type])
       glShaderSource(@id, 1, [source].pack('p'), [source.size].pack('I'))
       glCompileShader(@id)
+
+      if complile_error?
+        puts "Shader error."
+        puts error_log
+      end
+    end
+
+    def complile_error?
+      buf = '    '
+      glGetShaderiv(@id, GL_COMPILE_STATUS, buf)
+      buf.unpack('L')[0] == 0
+    end
+
+    def error_log
+      buf = '    '
+      glGetShaderiv(@id, GL_INFO_LOG_LENGTH, buf)
+      info_log_length = buf.unpack('L')[0]
+      buf = "%#{info_log_length}s" % " "
+      glGetShaderInfoLog(@id, 100, nil, buf)
+      buf
     end
   end
 end
